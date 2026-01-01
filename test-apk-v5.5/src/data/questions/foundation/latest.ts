069873e71b411777b532662254f67fb078aa38da
Akshaysir13 <akshaymoghe8@gmail.com>
2026-01-01 13:35:14 +0530

    Update latest.ts

--- test-apk-v5.5/src/data/questions/foundation/latest.ts
+++ test-apk-v5.5/src/data/questions/foundation/latest.ts
@@ -535,4 +535,553 @@ export const foundationlatestQuestions: Question[] = [
     correctOption: "d"
   },
   // PASTE YOUR 3000 PYQ QUESTIONS HERE
+     {
+    id: 1,
+    question: "Which of the Following Architect Design the Given Building",
+    image: "https://drive.google.com/thumbnail?id=1eWvmf56Gs_9Q6kfZlPA8SAaPc6GrbPya&sz=w1000",
+    optionA: "a",
+    optionB: "d",
+    optionC: "c",
+    optionD: "b",
+    correctOption: "d"
+  },
+  {
+    id: 2,
+    question: "Identify the famous Monument",
+    image: "https://drive.google.com/thumbnail?id=1z-OX_P4d79_PN5E3o63IkN6jdLjawS1Z&sz=w1000",
+    optionA: "Chinchen itza",
+    optionB: "HANGING GARDENS OF BABYLON.",
+    optionC: "GREAT PYRAMID OF GIZA",
+    optionD: "Stonehenge in england",
+    correctOption: "d"
+  },
+  {
+    id: 3,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_OL3eC434pFx_RAwCdablbE3CqIiGKfD&sz=w1000",
+    optionA: "a",
+    optionB: "c",
+    optionC: "b",
+    optionD: "d",
+    correctOption: "d"
+  },
+  {
+    id: 4,
+    type: 'statement',
+    question: "Statement 1: The color green is created by mixing blue and yellow.\nStatement 2: The color purple is created by mixing red and blue.",
+    optionA: "Statement 1 is False,  Statement 2 is True",
+    optionB: "Statement 1 is True,  Statement 2 is False",
+    optionC: "Both Statement 1 and Statement 2 is False",
+    optionD: "Both Statement 1 and Statement 2 are True.",
+    correctOption: "d"
+  },
+  {
+    id: 5,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_eRL4HI4ZK5Gxbvt3gNgLsh-i7cMf2r6&sz=w1000",
+    optionA: "c",
+    optionB: "d",
+    optionC: "a",
+    optionD: "b",
+    correctOption: "d"
+  },
+  {
+    id: 6,
+    type: 'statement',
+    question: "Statement 1: Vertical louvres are best for east and west-facing windows.\nStatement 2: They block high-angle sunlight from above.",
+    optionA: "S1 is false, but S2 is true",
+    optionB: "Both S1 and S2 are false",
+    optionC: "Both statements are true, and S2 is the correct explanation of S1",
+    optionD: "S1 is true, but S2 is false",
+    correctOption: "d"
+  },
+  {
+    id: 7,
+    question: "Which of the Following Architect Design the Given Building",
+    image: "https://drive.google.com/thumbnail?id=1doVaBLsltSy4LsF3esm9hnzHtj0-L5LO&sz=w1000",
+    optionA: "P N Mathur",
+    optionB: "B V Doshi",
+    optionC: "Shivnath Prasad",
+    optionD: "Charles Correa",
+    correctOption: "d"
+  },
+  {
+    id: 8,
+    question: "38. What is the primary purpose of applying plaster to a masonry wall?",
+    optionA: "To increase structural strength",
+    optionB: "To improve thermal insulation significantly",
+    optionC: "To act as a sound reflector",
+    optionD: "To provide a smooth, durable finish and protect the masonry",
+    correctOption: "d"
+  },
+  {
+    id: 9,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_gDTMvUqjLSBEEnSDabcWcTSMvyB7BIE&sz=w1000",
+    optionA: "a",
+    optionB: "b",
+    optionC: "d",
+    optionD: "c",
+    correctOption: "d"
+  },
+  {
+    id: 10,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_6eMGngH449nLNxgI-8l11Gx_okYO0Vq&sz=w1000",
+    optionA: "a",
+    optionB: "c",
+    optionC: "b",
+    optionD: "d",
+    correctOption: "d"
+  },
+  {
+    id: 11,
+    question: "Who built the Gol Gumbaz?",
+    optionA: "Shah Jahan",
+    optionB: "Akbar",
+    optionC: "sher shah suri",
+    optionD: "Muhammad Adil Shah",
+    correctOption: "d"
+  },
+  {
+    id: 12,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_Ev44QpRRnt1J2vx6jKbPt9gChePkDqa&sz=w1000",
+    optionA: "c",
+    optionB: "d",
+    optionC: "b",
+    optionD: "a",
+    correctOption: "d"
+  },
+  {
+    id: 13,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_61PCiAa-Ck3W7nQJchy3Y1ahK0VHS27&sz=w1000",
+    optionA: "a",
+    optionB: "d",
+    optionC: "b",
+    optionD: "c",
+    correctOption: "d"
+  },
+  {
+    id: 14,
+    question: "Who was the builder of the Shore Temple?",
+    optionA: "Chandela Dynasty",
+    optionB: "Rashtrakuta Dynasty",
+    optionC: "Chola Dynasty",
+    optionD: "Pallava Dynasty",
+    correctOption: "d"
+  },
+  {
+    id: 15,
+    question: "Which of the Following Architect Design the Given Building",
+    image: "https://drive.google.com/thumbnail?id=1HA3vxtUHzEpio_u_ntLQXjweZX2pVgvf&sz=w1000",
+    optionA: "d",
+    optionB: "a",
+    optionC: "b",
+    optionD: "c",
+    correctOption: "d"
+  },
+  {
+    id: 16,
+    type: 'match-pair',
+    question: "Match the Following",
+    columnAItems: [
+      "1. Colonial",
+      "2. Mughal",
+      "3. Indus Valley",
+      "4. Chinese"
+],
+    columnBItems: [
+      "- Grid-based city planning",
+      "- Pagoda-style roofs with upturned eaves",
+      "- European architectural influence",
+      "- Large domes"
+],
+    optionA: "1-D 2-B 3-A 4-C",
+    optionB: "1-A 2-C 3-D 4-B",
+    optionC: "1-D 2-B 3-A 4-C",
+    optionD: "1-C 2-D 3-A 4-B",
+    correctOption: "d"
+  },
+  {
+    id: 17,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_TQTsZssKunszucyz_1hmRglt1eQtgph&sz=w1000",
+    optionA: "d",
+    optionB: "c",
+    optionC: "b",
+    optionD: "a",
+    correctOption: "d"
+  },
+  {
+    id: 18,
+    question: "Who is an architect of Heydar Aliyev Centre",
+    optionA: "Norman Foster",
+    optionB: "Herber Baker",
+    optionC: "F L Wright",
+    optionD: "Zaha Hadid",
+    correctOption: "d"
+  },
+  {
+    id: 19,
+    type: 'match-pair',
+    question: "Match the Following",
+    columnAItems: [
+      "1. Guwahati",
+      "2. Ujjain",
+      "3. Bangalore",
+      "4. Kolkata"
+],
+    columnBItems: [
+      "Brahmaputra",
+      "Sipra",
+      "Vrishabhavathi",
+      "Hooghly"
+],
+    optionA: "1-A 2-D 3-C 4-B",
+    optionB: "1-C 2-A 3-D 4-B",
+    optionC: "1-B 2-C 3-D 4-A",
+    optionD: "1-A 2-B 3-C 4-D",
+    correctOption: "d"
+  },
+  {
+    id: 20,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_KOS8k-UDiQBXZWUBHt5ouSUHBnXI-QD&sz=w1000",
+    optionA: "a",
+    optionB: "c",
+    optionC: "d",
+    optionD: "b",
+    correctOption: "d"
+  },
+  {
+    id: 21,
+    question: "What is Meaning of a Load-Bearing Components?",
+    optionA: "To connect woodwork components in construction (doors, windows, etc.).",
+    optionB: "The part between ground level and floor level, protecting from moisture.",
+    optionC: "A suspended ceiling for aesthetics and insulation.",
+    optionD: "Structural elements like walls, beams, and columns that support loads.",
+    correctOption: "d"
+  },
+  {
+    id: 22,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_iTmiENetqJBfIza347ShRc2DKvoFI1y&sz=w1000",
+    optionA: "b",
+    optionB: "a",
+    optionC: "d",
+    optionD: "c",
+    correctOption: "d"
+  },
+  {
+    id: 23,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_9bDA_-L6zTKxP1dKqbketVWhUL9nldI&sz=w1000",
+    optionA: "a",
+    optionB: "b",
+    optionC: "d",
+    optionD: "c",
+    correctOption: "d"
+  },
+  {
+    id: 24,
+    type: 'statement',
+    question: "Statement 1: Philip Johnson designed the Glass House.\nStatement 2: The house is a minimalist structure with transparent walls.",
+    optionA: "Statement 1 is True,  Statement 2 is False",
+    optionB: "Statement 1 is False,  Statement 2 is True",
+    optionC: "Both Statement 1 and Statement 2 is False",
+    optionD: "Both Statement 1 and Statement 2 are True.",
+    correctOption: "d"
+  },
+  {
+    id: 25,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_jJZMsN56jJ6eP1kvddflq5XRXta0eZz&sz=w1000",
+    optionA: "12",
+    optionB: "11",
+    optionC: "10",
+    optionD: "9",
+    correctOption: "d"
+  },
+  {
+    id: 26,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_UveQOg-xTnX2HD1cearP5vZXXIkTPAG&sz=w1000",
+    optionA: "c",
+    optionB: "b",
+    optionC: "a",
+    optionD: "d",
+    correctOption: "d"
+  },
+  {
+    id: 27,
+    type: 'statement',
+    question: "Statement 1: A truss is a structural framework of triangular units.\nStatement 2: A cantilever is a beam supported at both ends.",
+    optionA: "Both Statement 1 and Statement 2 are True.",
+    optionB: "Statement 1 is False,  Statement 2 is True",
+    optionC: "Both Statement 1 and Statement 2 is False",
+    optionD: "Statement 1 is True,  Statement 2 is False",
+    correctOption: "d"
+  },
+  {
+    id: 28,
+    type: 'match-pair',
+    question: "Match the Following",
+    columnAItems: [
+      "1. Tezpur",
+      "2. Visakhapatnam",
+      "3. Bhagalpur",
+      "4. Bardhaman"
+],
+    columnBItems: [
+      "City of Blood",
+      "Silk City",
+      "City of Peace",
+      "City of Destiny"
+],
+    optionA: "1-B 2-D 3-A 4-C",
+    optionB: "1-C 2-B 3-A 4-D",
+    optionC: "1-A 2-C 3-B 4-D",
+    optionD: "1-A 2-D 3-B 4-C",
+    correctOption: "d"
+  },
+  {
+    id: 29,
+    question: "Which of the following is Primary Function of Roof?",
+    optionA: "To transfer loads safely to the ground and support the superstructure.",
+    optionB: "To provide ventilation, natural light, and external views.",
+    optionC: "To exert force on a structure, affecting its stability and strength.",
+    optionD: "To protect the structure from weather conditions and external elements.",
+    correctOption: "d"
+  },
+  {
+    id: 30,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_OdPXj2iOP_SBQ5fYdUY754E4mqeSkHp&sz=w1000",
+    optionA: "d",
+    optionB: "a",
+    optionC: "c",
+    optionD: "b",
+    correctOption: "d"
+  },
+  {
+    id: 31,
+    question: "NASA (Architecture) stands for?",
+    optionA: "National Architecture and Survy Association",
+    optionB: "National Association of Students of Architecture",
+    optionC: "New Architectural Student Association",
+    optionD: "National Association of Students of Architecture",
+    correctOption: "d"
+  },
+  {
+    id: 32,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_Bcp6GuxP6gDZJY3HkJT17rUqBYegaaE&sz=w1000",
+    optionA: "c",
+    optionB: "b",
+    optionC: "d",
+    optionD: "a",
+    correctOption: "d"
+  },
+  {
+    id: 33,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_T9hUFDciEZOWFIyJQU0DVGWE2evcJhR&sz=w1000",
+    optionA: "c",
+    optionB: "d",
+    optionC: "b",
+    optionD: "a",
+    correctOption: "d"
+  },
+  {
+    id: 34,
+    question: "What is Humayun’s Tomb known for in India?",
+    optionA: "Tallest dome",
+    optionB: "Largest courtyard",
+    optionC: "palace",
+    optionD: "First garden tomb",
+    correctOption: "d"
+  },
+  {
+    id: 35,
+    question: "Which type of paint is best for exterior surfaces?",
+    optionA: "Enamel Paint",
+    optionB: "Acrylic Paint",
+    optionC: "Distemper",
+    optionD: "Emulsion Paint",
+    correctOption: "d"
+  },
+  {
+    id: 36,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_5fBIP6xcaL649Ub-4fO0FKh5SSK8J-M&sz=w1000",
+    optionA: "b",
+    optionB: "c",
+    optionC: "d",
+    optionD: "a",
+    correctOption: "d"
+  },
+  {
+    id: 37,
+    question: "GBCI stands for?",
+    optionA: "Green Building Certificate Institute",
+    optionB: "Green Building Code of India",
+    optionC: "Government Building Control India",
+    optionD: "Green Building Certification Institute",
+    correctOption: "d"
+  },
+  {
+    id: 38,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_A6VecoWqKgG0AJ-Te4Fdn5uoim-HvTe&sz=w1000",
+    optionA: "19",
+    optionB: "18",
+    optionC: "16",
+    optionD: "17",
+    correctOption: "d"
+  },
+  {
+    id: 39,
+    question: "I love my job. What would I retire to? is Quated by ?",
+    optionA: "Francis Kéré",
+    optionB: "Norman Foster",
+    optionC: "Bjarke Ingels",
+    optionD: "Richard Rogers",
+    correctOption: "d"
+  },
+  {
+    id: 40,
+    type: 'match-pair',
+    question: "Match the Following",
+    columnAItems: [
+      "1. Shaft",
+      "2. Lintel",
+      "3. Landing",
+      "4. Threshold"
+],
+    columnBItems: [
+      "The main vertical part of a column.",
+      "A flat platform between stair flights.",
+      "A horizontal support above a window or door.",
+      "The strip at the bottom of a doorway."
+],
+    optionA: "1-D 2-A 3-C 4-B",
+    optionB: "1-A 2-C 3-D 4-B",
+    optionC: "1-D 2-C 3-A 4-B",
+    optionD: "1-A 2-C 3-B 4-D",
+    correctOption: "d"
+  },
+  {
+    id: 41,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_QlCE3mh4XfVQcE_Qs9yl69_iZE-mya7&sz=w1000",
+    optionA: "d",
+    optionB: "a",
+    optionC: "b",
+    optionD: "c",
+    correctOption: "d"
+  },
+  {
+    id: 42,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_5Lc6p0Y3uUI_T94cZd5LiBURMzRssQH&sz=w1000",
+    optionA: "c",
+    optionB: "a",
+    optionC: "d",
+    optionD: "b",
+    correctOption: "d"
+  },
+  {
+    id: 43,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_J_2oDIAUg4ypVCgHqkeZ8nncDriBVwU&sz=w1000",
+    optionA: "a",
+    optionB: "b",
+    optionC: "d",
+    optionD: "c",
+    correctOption: "d"
+  },
+  {
+    id: 44,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_g-maWmHIFehGvY2kRtZWdugbWobCqs3&sz=w1000",
+    optionA: "d",
+    optionB: "c",
+    optionC: "b",
+    optionD: "a",
+    correctOption: "d"
+  },
+  {
+    id: 45,
+    question: "The Poetics of Space is written By ?",
+    optionA: "Norman Foster",
+    optionB: "Bjarke Ingels",
+    optionC: "Stella Paul",
+    optionD: "Gaston Bachelard",
+    correctOption: "d"
+  },
+  {
+    id: 46,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_C6ekCSc3fU71Fw9VUyGktoL12dcEAcm&sz=w1000",
+    optionA: "c",
+    optionB: "d",
+    optionC: "a",
+    optionD: "b",
+    correctOption: "d"
+  },
+  {
+    id: 47,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_cKNck48mjoGzB7QuwaRPF04McD3Zww-&sz=w1000",
+    optionA: "a",
+    optionB: "c",
+    optionC: "b",
+    optionD: "d",
+    correctOption: "d"
+  },
+  {
+    id: 48,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1_iFcW1ivZIoSGrDc5a5uNt8rielfAXsJ&sz=w1000",
+    optionA: "a",
+    optionB: "b",
+    optionC: "d",
+    optionD: "c",
+    correctOption: "d"
+  },
+  {
+    id: 49,
+    type: 'match-pair',
+    question: "Match the Following",
+    columnAItems: [
+      "1. Gandhi Sadan",
+      "2. Laxminarayan Temple",
+      "3. Sidi Sayyad Mosque",
+      "4. Nagina Lake"
+],
+    columnBItems: [
+      "New Delhi",
+      "Ahmedabad",
+      "Vellore",
+      "Srinagar"
+],
+    optionA: "1-C 2-A 3-B 4-D",
+    optionB: "1-C 2-D 3-A 4-B",
+    optionC: "1-A 2-B 3-C 4-D",
+    optionD: "1-A 2-C 3-B 4-D",
+    correctOption: "d"
+  },
+  {
+    id: 50,
+    question: "Read the Instructions Carefully",
+    image: "https://drive.google.com/thumbnail?id=1etPY-k_mHB2M0WvCiWvWdqo_DHDJ3r8c&sz=w1000",
+    optionA: "b",
+    optionB: "d",
+    optionC: "c",
+    optionD: "a",
+    correctOption: "d"
+  },
+
 ];
