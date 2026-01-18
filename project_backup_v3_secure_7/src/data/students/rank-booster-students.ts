4e4107c2be45e6f7c09d40bf6889a47e56c98b5a
Akshaysir13 <akshaymoghe8@gmail.com>
2026-01-18 20:43:37 +0530

    Update rank-booster-students.ts

--- project_backup_v3_secure_7/src/data/students/rank-booster-students.ts
+++ project_backup_v3_secure_7/src/data/students/rank-booster-students.ts
@@ -70,11 +70,12 @@ export const rankBoosterStudents: UserAccount[] = [
 { email: 'tejasvi.sh@protonmail.com', password: 'India@2026#', role: 'student', approved: true , courses: ['rank_booster'] },
 //{ email: 'raunak8705@gmail.com', password: '801104', role: 'student', approved: true , courses: ['rank_booster'] },
 { email: 'kumarisakshi52905@gmail.com', password: 'Sakshi00', role: 'student', approved: true , courses: ['rank_booster'] },
-{ email: 'pihuriyanshu172@gmail.com ', password: 'Pihu@123', role: 'student', approved: true , courses: ['rank_booster'] },
-{ email: 'virajghule2224@gmail.com ', password: 'Viraj2224', role: 'student', approved: true , courses: ['rank_booster'] },
+{ email: 'pihuriyanshu172@gmail.com', password: 'Pihu@123', role: 'student', approved: true , courses: ['rank_booster'] },
+{ email: 'virajghule2224@gmail.com', password: 'Viraj2224', role: 'student', approved: true , courses: ['rank_booster'] },
 
 
 ];
+
 
 
 
