3b334cb80eecb89e7dad7c7d7d0e76fb0bcffa4d
Akshaysir13 <akshaymoghe8@gmail.com>
2026-03-18 14:28:48 +0530

    Update foundation-students.ts

--- project_backup_v3_secure_7/src/data/students/foundation-students.ts
+++ project_backup_v3_secure_7/src/data/students/foundation-students.ts
@@ -334,21 +334,6 @@ export const foundationStudents: UserAccount[] = [
 },
 
 //{ email: 'tanushsharma901@gmail.com', password: 'ramramji', role: 'student', approved: true, courses: ['foundation'] },
-//{ email: 'kumardevil077@gmail.com', password: 'Tirupati balaji temple ', role: 'student', approved: true, courses: ['foundation'] },
-  {
-  email: 'student67@gmail.com',
-  password: 'pass123',
-  role: 'student',
-  approved: true,
-  courses: ['foundation']
-},
-  {
-  email: 'student67@gmail.com',
-  password: 'pass123',
-  role: 'student',
-  approved: true,
-  courses: ['foundation']
-},
 {
   email: 'student68@gmail.com',
   password: 'pass123',
