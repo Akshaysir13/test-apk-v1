feb158e37ec40d12015f1245c66a75ebbc07a8cb
Akshaysir13 <akshaymoghe8@gmail.com>
2026-03-24 03:28:16 +0530

    Update latest.ts

--- project_backup_v3_secure_7/src/data/questions/foundation/latest.ts
+++ project_backup_v3_secure_7/src/data/questions/foundation/latest.ts
@@ -11860,7 +11860,7 @@ image: "https://drive.google.com/thumbnail?id=1V5C-agMFHEOecIu6XsJONOF8kXRbFEQf&
   },
   {
     id: 402,
-    question: "A kitchen counter contains items that suggest someone is baking — a mixing bowl, a wooden spoon, scattered flour, a recipe book, and a tray of unbaked cookies. Arrange these objects into a composition that tells the story of the baker. OR Sketch a unique eco-friendly greenhouse built into a hillside that reconnects with nature. Include features such as glass panels, wooden beams, climbing vines, and a rainwater collection system using natural materials. The structure should blend harmoniously with the surrounding landscape. Depict natural light, shadow, and textures effectively. Colour it as per your choice.",
+    question: "A kitchen counter contains items that suggest someone is baking — a mixing bowl, a wooden spoon, scattered flour, a recipe book, and a tray of unbaked cookies. Arrange these objects into a composition that tells the story of the baker. OR Sketch a unique eco-friendly greenhouse built into a hillside that reconnects with nature. Include features such as glass panels, wooden beams, climbing vines, etc. The structure should blend harmoniously with the surrounding landscape. Depict natural light, shadow, and textures effectively. Colour it as per your choice.",
     correctOption: "manual_evaluation"
   },
     
