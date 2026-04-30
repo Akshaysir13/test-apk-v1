90a206150babfdaca8a5e30a31569a70726a4b2d
Akshaysir13 <akshaymoghe8@gmail.com>
2026-04-30 15:36:19 +0530

    Add new students to nata2026Students array
    Added new students to the NATA 2026 course list.

--- project_backup_v3_secure_9 - NATA/src/data/students/nata-2026-students.ts
+++ project_backup_v3_secure_9 - NATA/src/data/students/nata-2026-students.ts
@@ -10,5 +10,18 @@ export const nata2026Students: UserAccount[] = [
   // Example:
   { email: 'nata300@gmail.com', password: 'pass123', role: 'student', approved: true, courses: ['nata_2026'] },
   { email: 'nata1@gmail.com', password: 'pass123', role: 'student', approved: true, courses: ['nata_2026'] },
+  { email: 'akshagarwal2102@gmail.com', password: 'Nata210', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'om.mandal146@gmail.com', password: '9911527519', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'dzanamika08@gmail.com', password: 'CH3COONa!', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'aleena@gmail.com', password: 'aleena2008', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'archinuzhatkhan@gmail.com', password: 'Khan1234', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'tanshusurya@gmail.com', password: 'tanshu2212', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'fatimanuzhat2007@gmail.com', password: 'Mogh4321', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'saanvigoel08@gmail.com', password: 'saanvi08', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'sherylshivangi@gmail.com', password: 'idkwhymefr', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'niranjanmehta105@gmail.com', password: 'Aman#1996', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'aryanjojare8@gmail.com', password: 'Aryan0230', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'priyadarshnithiru@gmail.com', password: 'priyuthiru', role: 'student', approved: true, courses: ['nata_2026'] },
+{ email: 'teezas611@gmail.com', password: 'FIREFREE#1', role: 'student', approved: true, courses: ['nata_2026'] },
 ];
 
